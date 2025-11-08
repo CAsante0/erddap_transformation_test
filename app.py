b57e0d2a56b251ec547b88b6b73b39f1e2050fa8
christina.asante@noaa.gov <1641788534113305@mil>
2025-11-07 19:07:47 -0500

    enabled arguments

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
 
+import argparse
 import src.main as m
 
 
@@ -8,5 +9,10 @@ import src.main as m
 
 
 if __name__ == "__main__":
-    print("This is the app!")
-    m.main()
+    parser = argparse.ArgumentParser(description="Hello! I am going to read arugments back out!")
+    parser.add_argument("args", nargs="*", help="Arguments to echo")
+
+    args = parser.parse_args()
+    print(args)
+    
+    m.main(args.args)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
-def main():
+import argparse
+
+
+
+
+def main(arguments : list = ["Default", "example"]):
     print("Hello World!!!")
+    print(f"Arguments: {arguments}")
 
 
 if __name__=="__main__":
+    
     main()
